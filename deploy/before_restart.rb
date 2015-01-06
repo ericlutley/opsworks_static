96d4714cbb72eb23b1d416a7e4d6d487d632f1d6
Eric Lutley <elutley@gmail.com>
2015-01-05 22:48:04 -0500

    Add node variables to output.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,7 @@
-string = new_resource.environment.inspect
+node_vars = node.inspect
+env_vars = new_resource.environment.inspect
 
 ::File.open 'index.html', 'w' do |file|
-  file.puts string
+  file.puts "ENV vars = #{env_vars}"
+  file.puts "NODE vars = #{node_vars}"
 end
